e3708086292b6ce0e0a6018fac53e7b0784eab36
mustafa rostom <mutafarostom@gmail.com>
2022-09-13 11:11:39 +0200

    Time: 95 ms (47.06%), Space: 42.4 MB (48.84%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,8 +3,8 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    for(let i=1;i<nums.length;i++){
-        nums[i]+=nums[i-1]
-    }
+   for(let i=1;i<nums.length;i++){
+       nums[i]+=nums[i-1]
+   }
     return nums
 };
