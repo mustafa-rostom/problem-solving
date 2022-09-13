7878825456691f09732345d084b6d9c912ecbf22
mustafa rostom <mutafarostom@gmail.com>
2022-09-13 11:31:59 +0200

    Time: 133 ms (41.10%), Space: 44.1 MB (87.16%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -3,13 +3,10 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
+   
     let totalSum=0,
         leftSum=0;
-    
-    nums.forEach((e)=>{
-        totalSum+=e
-    })
-    
+    nums.forEach((e)=>totalSum+=e)
     for(let i=0;i<nums.length;i++){
         if(totalSum-leftSum-nums[i]===leftSum){
             return i
