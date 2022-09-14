2d39fbbda48f545357af42a171d6586f7457bdd4
mustafa rostom <mutafarostom@gmail.com>
2022-09-14 11:17:39 +0200

    Time: 132 ms (29.11%), Space: 42.5 MB (80.41%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.js
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -4,21 +4,21 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-      if (s.length !== t.length) {
-        return false;
-    }
-    if (s === t) {
-        return true;
-    }
-    const obj1 = {};
-    const obj2 = {};
-    for(let i = 0; i < s.length; i++) {
-        if(obj1[s[i]] !== obj2[t[i]]){
-            return false
-        }else{
-            obj1[s[i]]=i;
-            obj2[t[i]]=i
-        }
-    }
-    return true;
+  if (s.length !== t.length) {
+   return false;
+ }
+ if (s === t) {
+   return true;
+ }
+ const obj1 = {};
+ const obj2 = {};
+ for (let i = 0; i < s.length; i++) {
+   if (obj1[s[i]] !== obj2[t[i]]) {
+     return false;
+   } else {
+     obj1[s[i]] = i;
+     obj2[t[i]] = i;
+   }
+ }
+ return true;
 };
